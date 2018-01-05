b0d83c02a71e9d244b9a16283a37bc2e2347c740
cacacai <2445365071@qq.com>
2018-01-05 11:01:25 +0800

    增加美颜功能

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -7,6 +7,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
@@ -14,16 +15,12 @@ import org.opencv.highgui.Highgui;
 import org.opencv.objdetect.CascadeClassifier;
 
 import net.coobird.thumbnailator.Thumbnails;
-
-//
-// Detects faces in an image, draws boxes around them, and writes the results
-// to "faceDetection.png".
-//
 public class Main {
 	private static String url="D:/image";
 	private static String saveUrl="D:/image/newimage/";
 	
 	public void run() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
     File dir = new File(url);
     /**
      * 读取图片路径
@@ -47,6 +44,14 @@ public class Main {
 	 */
 	for (File file : images) {
 		//smartData(file);
+		if (file.length() > 1000000) {
+			try {
+				Thumbnails.of(file).scale(1f).outputQuality(0.8f).toFile(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		//dealImage(file);
 	}
 	
@@ -64,18 +69,18 @@ public class Main {
 	});
 	
 	for (File file : newImages) {
-		setBGC(file);
-//		try {
-//			//一寸
-//			Thumbnails.of(file).size(259, 341).keepAspectRatio(false).toFile(file);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}  
+			//
+			try {
+				// 一寸
+				Thumbnails.of(file).size(259, 413).keepAspectRatio(true).toFile(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			setBGC(file);
 		}
-	
-	
-  }
+
+	}
 
 /**
  * 压缩图片
@@ -87,7 +92,7 @@ public class Main {
 		
 		if (file.length() > 1000000) {
 			try {
-				Thumbnails.of(file).scale(1f).outputQuality(0.25f).toFile(file);
+				Thumbnails.of(file).scale(1f).outputQuality(0.8f).toFile(file);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -100,13 +105,6 @@ public class Main {
 	 * @param file
 	 */
 	private void setBGC(File image) {
-		 /** 
-         * 定义一个RGB的数组，因为图片的RGB模式是由三个 0-255来表示的 比如白色就是(255,255,255) 
-         */  
-        int[] rgb = new int[3];  
-        /** 
-         * 用来处理图片的缓冲流 
-         */  
         BufferedImage bi = null;  
         try {  
             /** 
@@ -123,52 +121,109 @@ public class Main {
         int height = bi.getHeight();  
         int minx = bi.getMinX();  
         int miny = bi.getMinY();  
+        int rightHeight=0;  //右边最低点
+        int leftHeight=0;   //左边最低点
+        int flage;
+        int temp=0;
+        int bfHeiht = 0;
+        boolean f=false;
         System.out.println("正在处理："+image.getName());  
-        /** 
-         * 这里是遍历图片的像素，因为要处理图片的背色，所以要把指定像素上的颜色换成目标颜色 
-         * 这里 是一个二层循环，遍历长和宽上的每个像素 
-         */  
+        for (int i = 0; i < height; i++) {
+            if(isWhite(bi,0,i)){
+            	leftHeight=i;
+            }
+		}
+        for (int i = 0; i < height; i++) {
+            if(isWhite(bi,width-1,i)){
+            	rightHeight=i;
+            }
+		}
         /**
-         * 水平1/4 3/4
-         * 垂直1/2
+         * 从上到下
+         * 中间-》左
          */
-        int widthOneToFor=(int)width/5;
-        int widthThreeToFor=4*widthOneToFor;
-        //左边
-        int tempHeight = 0;
-        for (int i = minx; i <=width/2; i++) {  
-            for (int j = miny; j < height; j++) {  
-                // System.out.print(bi.getRGB(jw, ih));  
-                /** 
-                 * 得到指定像素（i,j)上的RGB值， 
-                 */  
-                int pixel = bi.getRGB(i, j);  
-                /** 
-                 * 分别进行位操作得到 r g b上的值 
-                 */  
-                rgb[0] = (pixel & 0xff0000) >> 16;  
-                rgb[1] = (pixel & 0xff00) >> 8;  
-                rgb[2] = (pixel & 0xff);  
-                
-                /** 
-                 * 进行换色操作，我这里是要把蓝底换成白底，那么就判断图片中rgb值是否在蓝色范围的像素 
-                 */  
-                if(rgb[0]<240&&rgb[0]>=160 && rgb[1]<240&&rgb[1]>=160 && rgb[2]<240&&rgb[2]>=160){
-                	if (i==minx) {
-						tempHeight=j;//找到左边最低
-					}else if(j>tempHeight) {
-						continue;
-					}
-                    /** 
-                     * 这里是判断通过，则把该像素换成白色 
-                     */ 
-                    bi.setRGB(i, j, 0x0066CC);  
-                }else if((j<(width/3)*2)&&(i<widthOneToFor||i>widthThreeToFor)){
-                	bi.setRGB(i, j, 0x0066CC);
+        
+        for (int i = (int)width/2; i>=minx; i--) {
+        	temp=0;
+            for (int j = miny; j < height; j++) { 
+            	if (j-bfHeiht>100) {//当前的j比之前的j相差范围过大就表示走到了脸的最左侧
+					f=true;
+				}
+            	if (j-temp>5) { //如果当前的j和同一竖线下的前一个j不连续而且相差大于5就表示以及到达边缘了
+            		bfHeiht=j;
+					break;
+				}
+            	//&&(((i<width/2&&j<leftHeight)||(i>width/2&&j>rightHeight)))
+                if(isWhite(bi,i,j)){
+                		bi.setRGB(i, j, 0x0066CC);
+                		temp=j;
                 }
-                  
+            }
+            if (f) {//如果脸的最右侧就退出
+            	f=false;
+				break;
+			}
+        } 
+        /**
+         * 上-》下
+         * 中-》右
+         */
+        bfHeiht=0;
+        for (int i = (int)width/2; i<width; i++) {
+        	temp=0;
+            for (int j = miny; j < height; j++) { 
+            	if (j-bfHeiht>100) {//当前的j比之前的j相差范围过大就表示走到了脸的最左侧
+					f=true;
+				}
+            	if (j-temp>5) { //如果当前的j和同一竖线下的前一个j不连续而且相差大于5就表示以及到达边缘了
+            		bfHeiht=j;
+					break;
+				}
+            	//&&(((i<width/2&&j<leftHeight)||(i>width/2&&j>rightHeight)))
+                if(isWhite(bi,i,j)){
+                		bi.setRGB(i, j, 0x0066CC);
+                		temp=j;
+                }
+            }
+            if (f) {//如果脸的最右侧就退出
+            	f=false;
+				break;
+			}
+        }
+        //从左往右
+        for (int i = miny; i <height; i++) {  
+        	temp=0;
+        	flage=0;
+            for (int j =minx ; j <=width/2 ; j++) {  
+            	if (j-temp>3&&flage!=0) {
+					break;
+				}
+                if(isWhite(bi,j,i)&&i<leftHeight){
+                	flage++;
+                	bi.setRGB(j, i, 0x0066CC);
+                	temp=j;
+                }
+                                  
+            }  
+        }
+        
+        //从右往左
+        for (int i = miny; i <height; i++) {  
+        	temp=0;
+        	flage=0;
+            for (int j =width-1 ; j >=width/2 ; j--) {  
+            	if (temp-j>3&&flage!=0) {
+					break;
+				} 
+                if(isWhite(bi,j,i)&&i<rightHeight){
+                	flage++;
+                	bi.setRGB(j, i, 0x0066CC);
+                	temp=j;
+                }
+                                 
             }  
         }  
+        
         System.out.println("\t处理完毕："+image.getName());  
         System.out.println();  
         /** 
@@ -176,7 +231,7 @@ public class Main {
          */  
         FileOutputStream ops = null;
 		try {
-			ops = new FileOutputStream(new File("D:/newImage/"+image.getName()+".jpg"));
+			ops = new FileOutputStream(new File("D:/newImage/"+image.getName()));
 			ImageIO.write(bi,"jpg", ops);
 			ops.flush();
 			ops.close();
@@ -188,6 +243,28 @@ public class Main {
 	
 	}
 	
+	/**
+	 * 颜色判断
+	 * @param file
+	 */
+	private boolean isWhite(BufferedImage image,int i,int j) {
+		int pixel = image.getRGB(i, j);  
+        /** 
+         * 分别进行位操作得到 r g b上的值 
+         */  
+		int[] rgb=new int[3];
+        rgb[0] = (pixel & 0xff0000) >> 16;  
+        rgb[1] = (pixel & 0xff00) >> 8;  
+        rgb[2] = (pixel & 0xff);  
+        
+        /** 
+         * 进行换色操作，我这里是要把蓝底换成白底，那么就判断图片中rgb值是否在蓝色范围的像素 
+         */  
+        if(rgb[0]<240&&rgb[0]>=160 && rgb[1]<240&&rgb[1]>=160 && rgb[2]<240&&rgb[2]>=160){
+        	return true;
+        }
+		return false;
+	}
 	//图片处理
 	private void dealImage(File file) {
 		Rect face=detectFace(file);
@@ -198,9 +275,6 @@ public class Main {
 			Thumbnails.of(file).sourceRegion((int) (face.x - face.width * 0.4),
 					(int) (face.y - face.height * 0.6),
 					(int) (face.width * 1.8), (int) (face.height * 2.4)).scale(1f).toFile(saveUrl+file.getName());
-			System.out.println((int) (face.x - face.width * 0.4)+" "+
-					(int) (face.y - face.height * 0.6)+" "+
-					(int) (face.width * 1.8)+" "+(int) (face.height * 2.4));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -3,5 +3,6 @@ package com.java;
 public class Test {
 	public static void main(String[] args) {
 		new Main().run();
+		new BeautyFace().run();
 	}
 }
